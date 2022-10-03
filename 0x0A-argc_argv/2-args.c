d1eb0ad30c0fb33c19a31f6f0fa704e97700cc1a
Terrence Chipuka <kuwawoga@outlook.com>
2022-10-03 20:28:14 +0200

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - program that prints the number of arguments passed into it
+ * main - program that multiplies two numbers.
  * @argc: number of arguments
  * @argv: array with the arguments
  *
@@ -10,8 +11,15 @@
 
 int main(int argc, char *argv[])
 {
-	(void)argv;
+	int mult = 0;
+	char c[] = "Error";
 
-	printf("%d\n", argc - 1);
+	if (argc != 3)
+	{
+		printf("%s\n", c);
+		return (1);
+	}
+	mult = atoi(argv[1]) * atoi(argv[2]);
+	printf("%i\n", mult);
 	return (0);
 }
